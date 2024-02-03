ae644c60fc5f727157b1614aa1e78daa88804868
Jake Scaife <jakescaife@pm.me>
2024-02-03 11:11:17 +0000

    Add remaps

--- init.lua
+++ init.lua
@@ -12,4 +12,5 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 require("vim-settings")
+require("remaps")
 require("lazy").setup("plugins")
---
+++ lua/remaps.lua
@@ -0,0 +1,20 @@
+-- Move lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- Keep cursor in position
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste / Delete to void register
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>Y", "\"+Y")
