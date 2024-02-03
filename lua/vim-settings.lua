4834ad2197e8ae3670050712e4d3531b94e2f191
Jake Scaife <jakescaife@pm.me>
2024-02-03 10:55:15 +0000

    Update editor settings

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -1,6 +1,28 @@
-vim.cmd("set noexpandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number relativenumber")
-vim.cmd("set nowrap")
+vim.opt.guicursor = ""
 vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
