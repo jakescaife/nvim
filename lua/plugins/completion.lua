ddb1f6b0829e6da74cdc83c8488562c5200c43cd
Jake Scaife <jakescaife@pm.me>
2024-02-04 15:53:31 +0000

    Add nvim-cmp/luasnip tabbing between placeholders

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -13,11 +13,12 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+			local snip = require("luasnip")
 			require("luasnip.loaders.from_vscode").lazy_load()
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						snip.lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -30,6 +31,20 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if snip.expand_or_jumpable() then
+							snip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if snip.jumpable(-1) then
+							snip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
