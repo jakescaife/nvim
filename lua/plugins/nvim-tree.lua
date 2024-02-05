b6b9dda7a16e60dd369caf70e487580311cfdf35
Jake Scaife <jakescaife@pm.me>
2024-02-05 16:21:43 +0000

    Set nvim-tree to autofocus files

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,7 +4,11 @@ return {
 	lazy = false,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("nvim-tree").setup({})
+		require("nvim-tree").setup({
+			update_focused_file = {
+				enable = true,
+			},
+		})
 		vim.keymap.set("n", "<C-n>", "<cmd> NvimTreeToggle <CR>", { desc = "Toggle nvim-tree" })
 		vim.keymap.set("n", "<leader>e", "<cmd> NvimTreeFocus <CR>", { desc = "Focus nvim-tree" })
 	end,
